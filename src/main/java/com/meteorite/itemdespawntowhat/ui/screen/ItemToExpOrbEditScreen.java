672a18d225d2ed693a6faee05e6eae546ffaf581
meteorite_l <2569390034@qq.com>
2026-03-18 22:41:10 +0800

    优化转化为实体（进行中 3/?）

--- src/main/java/com/meteorite/itemdespawntowhat/ui/screen/ItemToExpOrbEditScreen.java
+++ src/main/java/com/meteorite/itemdespawntowhat/ui/screen/ItemToExpOrbEditScreen.java
@@ -3,7 +3,7 @@ package com.meteorite.itemdespawntowhat.ui.screen;
 import com.meteorite.itemdespawntowhat.config.conversion.ItemToExpOrbConfig;
 import net.minecraft.client.gui.components.EditBox;
 
-public class ItemToExpOrbEditScreen extends BaseConfigEditScreen<ItemToExpOrbConfig>{
-    private EditBox
-
-}
+//public class ItemToExpOrbEditScreen extends BaseConfigEditScreen<ItemToExpOrbConfig>{
+//    private EditBox
+//
+//}
